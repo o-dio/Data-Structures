682ced6a89835084a884a349d1cc442a007bf74b
Dioniso <dionisoantunes@gmail.com>
2025-05-12 16:28:47 -0300

    Update Selection-Sort.C

--- Selection-Sort.C
+++ Selection-Sort.C
@@ -1,11 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define N 10;
+
 void selectionSort(int* list, int size);
 
 int main(){
 
+  int list[N], i;
   
+  for(i = 0; i < N; i++){
+    list[i] = N - i;
+    printf("%d\n", list[i]);
+  }
+  
+  selectionSort(list, N);
+
+  printf("------------\n");
+  
+  for(i = 0; i < N; i++){
+    printf("%d\n", list[i]);
+  }
 
   return 0;
 }
